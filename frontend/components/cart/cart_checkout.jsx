0e9d31b904e7edd8dbabd2f13f807b1b46d39a94
Shanelle Valencia <sgev808@hawaii.edu>
2020-10-12 11:38:47 -0700

    Create cart checkout component

---
+++ frontend/components/cart/cart_checkout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+class CartCheckout extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
+export default CartCheckout;
