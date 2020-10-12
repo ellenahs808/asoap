983aa0616f50096da2f095aa6df1ab2494aaa6c6
Shanelle Valencia <sgev808@hawaii.edu>
2020-10-11 21:57:09 -0700

    Add carts component

---
+++ frontend/components/cart/cart.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+class Cart extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
+export default Cart;
